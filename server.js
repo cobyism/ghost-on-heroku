d378fbd6cb2b37ba64da5be2c61f3947f4c56dab
Antal Orcsik <aorcsik@gmail.com>
2016-09-16 08:33:23 +0200

    upgrade ghost to 0.11.0

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// Temporary fix for lodash issue: https://github.com/TryGhost/Ghost/issues/7336
-require('ghost/core/server/overrides');
-
 var path = require('path');
 var ghost = require('ghost');
 
