590091d6473b24b1eb476eebefe32d27bf8cee7b
Ramin B <ramin@wework.com>
2018-05-30 13:31:13 -0400

    closer

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
-var ghost = require('ghost');
 var cluster = require('cluster');
+var ghost = require('ghost');
+var express = require('express');
+var parentApp = express();
 
 // Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
 var WORKERS = process.env.WEB_CONCURRENCY || 1;
@@ -17,16 +19,17 @@ if (cluster.isMaster) {
 } else {
   // Run Ghost in each worker / processor core.
   ghost().then(function (ghostServer) {
+    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
     //
-    ghostServer.start();
-
-    // write nginx tmp
-    fs.writeFile("/tmp/app-initialized", "Ready to launch nginx", function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("The file was saved!");
-      }
+    ghostServer.start(parentApp).then(function () {
+      // write nginx tmp
+      fs.writeFile("/tmp/app-initialized", "Ready to launch nginx", function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("The file was saved!");
+        }
+      });
     });
   });
 }
