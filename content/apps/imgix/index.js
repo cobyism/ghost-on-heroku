b4e337c62a25f46e8d4edfee3697729baf24a68d
Ramin B <ramin@wework.com>
2018-06-22 13:38:52 -0400

    img diff

--- content/apps/imgix/index.js
+++ content/apps/imgix/index.js
@@ -6,10 +6,24 @@ const client = new ImgixClient({
   host: 'labs-ghost.imgix.net'
 })
 
+const defaultParams = {
+  auto: 'compress,format',
+  ch: 'DPR,Width',
+  dpr: 2, // default to 2, client-hint will override
+}
+
 module.exports = App.extend({
   activate: ghost => {
     ghost.helpers.register('imgix_url', (id, params) => {
-      return client.buildURL(id, parse(params.replace(/'/g, '"')))
+      const path = id.replace(process.env.ASSET_HOST_URL, '')
+      const parsedParams = parse(params.replace(/'/g, '"'))
+      const imgixParams = Object.assign({}, parsedParams, defaultParams)
+      const url = client.buildURL(path, imgixParams)
+
+      console.log('************* imgix_url', { id, path, params, parsedParams, imgixParams, url })
+
+
+      return url
     })
   }
 })
