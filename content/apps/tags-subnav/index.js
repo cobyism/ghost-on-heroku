ce63b52857410bdb36b5dec1e937d34b70052529
Ramin B <ramin@wework.com>
2018-07-23 07:57:50 -0400

    logs

--- content/apps/tags-subnav/index.js
+++ content/apps/tags-subnav/index.js
@@ -1,3 +1,4 @@
+const uniq = require('lodash.uniq')
 const uniqBy = require('lodash.uniqby')
 const sortBy = require('lodash.sortby')
 const get = require('lodash.get')
@@ -15,16 +16,19 @@ module.exports = App.extend({
       } = data
       const { slug: currentSlug } = tag
 
-      const currentUniqTags = uniqBy(
-        flatten(currentPosts.map(post => post.tags)),
-        'slug'
-      )
-      const currentSlugs = currentUniqTags.map(tag => tag.slug)
+      const currentTags = flatten(currentPosts.map(post => post.tags))
+      const currentSlugs = currentTags.map(tag => tag.slug)
+      const currentUniqSlugs = uniq(currentSlugs)
+
+      console.log('******** currentSlugs', currentSlugs)
+      console.log('******** currentUniqSlugs', currentUniqSlugs)
 
       const relevantTags = []
       allPosts.forEach(post => {
         const slugs = post.tags.map(tag => get(tag, 'slug'))
         const slugIntersection = intersection(slugs, currentSlugs)
+
+        console.log('******** slugIntersection', { slugs, currentSlugs })
 
         if (slugIntersection.length > 0) {
           relevantTags.push(post.tags)
