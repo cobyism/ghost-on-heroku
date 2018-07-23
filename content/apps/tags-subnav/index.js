fab63d7642739cbbab3f2a9e78edf33d34a9cff3
Ramin B <ramin@wework.com>
2018-07-22 23:41:57 -0400

    subnavs ftw

--- content/apps/tags-subnav/index.js
+++ content/apps/tags-subnav/index.js
@@ -1,19 +1,45 @@
 const uniqBy = require('lodash.uniqby')
+const sortBy = require('lodash.sortby')
+const get = require('lodash.get')
+const flatten = require('lodash.flatten')
+const intersection = require('lodash.intersection')
 const App = require('ghost-app')
+
+const topLevelTagSlugs = ['startup-101', 'foo']
 
 module.exports = App.extend({
   activate: ghost => {
-    ghost.helpers.register('tags_subnav', (posts, options) => {
+    ghost.helpers.register('tags_subnav', options => {
+      const { data } = options
+      const { posts: allPosts } = options.hash
       const {
-        hash: { slug }
-      } = options
-      const allTags = posts.reduce((acc, post) => acc.concat(post.tags), [])
-      const uniqTags = uniqBy(allTags, 'slug')
-      const navTags = uniqTags.filter(tag => tag.slug !== slug)
+        root: { tag, posts: currentPosts }
+      } = data
+      const { slug: currentSlug } = tag
 
-      console.log('******** TAGS.tags', navTags)
+      const currentUniqTags = uniqBy(
+        flatten(currentPosts.map(post => post.tags)),
+        'slug'
+      )
+      const currentSlugs = currentUniqTags.map(tag => tag.slug)
 
-      return navTags
+      const relevantTags = []
+      allPosts.forEach(post => {
+        const slugs = post.tags.map(tag => get(tag, 'slug'))
+        const slugIntersection = intersection(slugs, currentSlugs)
+
+        if (slugIntersection.length > 0) {
+          relevantTags.push(post.tags)
+        }
+      })
+
+      const allUniqTags = uniqBy(flatten(relevantTags), 'slug')
+      const subnavTags = allUniqTags.filter(tag => !topLevelTagSlugs.includes(tag.slug))
+
+      // console.log('********* subnavTags', subnavTags)
+      // console.log('********* allUniqTags', allUniqTags)
+
+      return options.fn({ tags: sortBy(subnavTags, 'slug') })
     })
   }
 })
