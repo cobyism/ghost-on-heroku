53a39b8ae2b79cc4c97b8ac8112993826bad6a6e
Mars Hall <mars@vurt.com>
2019-02-09 16:09:47 -0800

    Ghost is strictly single-process

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 var ghost = require('ghost');
-var cluster = require('cluster');
 
-// Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
-var WORKERS = process.env.WEB_CONCURRENCY || 1;
-
-if (cluster.isMaster) {
-  // Master starts all workers and restarts them when they exit.
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`);
-    cluster.fork();
+// Run a single Ghost process
+ghost()
+  .then( ghostServer => ghostServer.start() )
+  .catch( error => {
+    console.error(`Ghost server error: ${error.message} ${error.stack}`);
+    process.exit(1);
   });
-  for (var i = 0; i < WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  // Run Ghost in each worker / processor core.
-  ghost().then(function (ghostServer) {
-    ghostServer.start();
-  });
-}
