c577fc31e9db0b92b45c8d6e249520891ae86b57
Noah Prail <nprail@schneidergis.com>
2019-10-30 16:59:05 -0400

    chore: Upgrade to Ghost 3.0

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var ghost = require('ghost');
 
 // Run a single Ghost process
 ghost()
-  .then( ghostServer => ghostServer.start() )
-  .catch( error => {
+  .then(ghostServer => ghostServer.start())
+  .catch(error => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
   });
